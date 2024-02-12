8b7dbd1f9e673ce0f59bf1238e0c4540fe3b213b
Temi Taylor <rocktalonttaylor@gmail.com>
2024-02-12 18:14:56 -0500

    sorry i'm sleepty

--- scenes/game/local_game.gd
+++ scenes/game/local_game.gd
@@ -1732,7 +1732,8 @@ class Player:
 			else:
 				events += reshuffle_discard(false)
 				if not parent.game_over:
-					draw_from_index = len(deck)-1
+					if from_bottom:
+						draw_from_index = len(deck)-1
 					var card = deck[0]
 					hand.append(card)
 					deck.remove_at(draw_from_index)
