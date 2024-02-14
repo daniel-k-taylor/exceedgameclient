6f60ab928a782dd754ffd677d517695ff4a97f22
Temi Taylor <rocktalonttaylor@gmail.com>
2024-02-13 22:47:45 -0500

    Disabling Rachel selection

--- globals/card_definitions.gd
+++ globals/card_definitions.gd
@@ -67,6 +67,8 @@ func _ready():
 			continue
 		var deck_data = load_json_file(decks_path + "/" + deck_file)
 		if deck_data:
+			if deck_data['id'] == "rachel":
+				continue #TODO
 			decks.append(deck_data)
 
 func get_card(definition_id):
