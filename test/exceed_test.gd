19e6365696f054d0d210533c6327c3bfe9f85bc7
Charles Tam <rngesus.wept+github@gmail.com>
2024-04-05 15:22:43 -0400

    Remake core exceed test features
    Test card IDs are now generated automatically with an incrementing counter, instead of being
    limited to the allotment hard-coded into the test suite. The downside is that you have to
    assign the ids to variables if you ever want to refer to them again. The upside is
    this guarantees no collisions on reused card IDs.
    
    Player choices during a strike are now all listed in a single array per player. It is up to
    the test author to ensure that the arguments provided are appropriate to the sequence
    of decisions that will be presented during the test case. The upshot is that whenever a new
    kind of decision needs to be made for a test, its parameters can simply be added to the
    decisions array, and handling for that decision type is easy to add through the match
    statements in process_decisions and process_remaining_decisions. This replaces the need to
    alter the function signature of execute_strike with new arguments each time a new kind of
    decision needs to be supported.

--- test/exceed_test.gd
+++ test/exceed_test.gd
@@ -2,11 +2,7 @@ class_name ExceedGutTest
 extends GutTest
 
 var game_logic : LocalGame
-const TestCardId1 = 50001
-const TestCardId2 = 50002
-const TestCardId3 = 50003
-const TestCardId4 = 50004
-const TestCardId5 = 50005
+var next_test_card_id = 50000
 
 var player1 : LocalGame.Player
 var player2 : LocalGame.Player
@@ -15,6 +11,10 @@ var player2 : LocalGame.Player
 # Subclasses must override this function
 func who_am_i():
 	return "no-one-lmao"
+
+func next_id():
+	next_test_card_id += 1
+	return next_test_card_id - 1
 
 func default_game_setup(alt_opponent : String = ""):
 	var default_deck = CardDefinitions.get_deck_from_str_id(who_am_i())
@@ -31,27 +31,34 @@ func default_game_setup(alt_opponent : String = ""):
 	player2 = game_logic.opponent
 	game_logic.get_latest_events()
 
-func give_player_specific_card(player, def_id, card_id):
+func give_player_specific_card(player, def_id):
 	var card_def = CardDefinitions.get_card(def_id)
+	var card_id = next_id()
 	var card = GameCard.new(card_id, card_def, "image", player.my_id)
 	var card_db = game_logic.get_card_database()
 	card_db._test_insert_card(card)
 	player.hand.append(card)
+	return card_id
 
 func give_specific_cards(p1, id1, p2, id2):
+	var test_ids = []
 	if p1 and id1:
-		give_player_specific_card(p1, id1, TestCardId1)
+		test_ids.append(give_player_specific_card(p1, id1))
 	if p2 and id2:
-		give_player_specific_card(p2, id2, TestCardId2)
+		test_ids.append(give_player_specific_card(p2, id2))
+	return test_ids
 
 func position_players(p1, loc1, p2, loc2):
 	p1.arena_location = loc1
 	p2.arena_location = loc2
 
 func give_gauge(player, amount):
+	var card_ids = []
 	for i in range(amount):
 		player.add_to_gauge(player.deck[0])
+		card_ids.append(player.deck[0].id)
 		player.deck.remove_at(0)
+	return card_ids
 
 func validate_has_event(events, event_type, target_player, number = null):
 	for event in events:
@@ -105,8 +112,8 @@ func do_and_validate_strike(player, card_id, ex_card_id = -1):
 	else:
 		fail_test("Unexpected game state after strike")
 
-func do_strike_response(player, card_id, ex_card = -1):
-	assert_true(game_logic.do_strike(player, card_id, false, ex_card))
+func do_strike_response(player, card_id, ex_card_id = -1):
+	assert_true(game_logic.do_strike(player, card_id, card_id == -1, ex_card_id))
 	var events = game_logic.get_latest_events()
 	return events
 
@@ -137,84 +144,96 @@ func validate_discard(player, amount, id):
 			return
 	fail_test("Didn't have required card in discard.")
 
-func handle_simultaneous_effects(initiator, defender, simul_effect_choices : Array):
-	while game_logic.game_state == Enums.GameState.GameState_PlayerDecision and game_logic.decision_info.type == Enums.DecisionType.DecisionType_ChooseSimultaneousEffect:
-		var decider = initiator
-		if game_logic.decision_info.player == defender.my_id:
-			decider = defender
-		var choice = 0
-		if len(simul_effect_choices) > 0:
-			choice = simul_effect_choices[0]
-			simul_effect_choices.remove_at(0)
-		assert_true(game_logic.do_choice(decider, choice), "Failed simuleffect choice")
+func process_decisions(player, strike_state, decisions):
+	while game_logic.game_state == Enums.GameState.GameState_PlayerDecision and \
+			game_logic.active_strike.strike_state == strike_state and \
+			game_logic.decision_info.player == player.my_id:
+		if not decisions:
+			fail_test("Insufficient decisions defined for player %s during strike" % player)
+		var content = decisions.pop_front()
+		print(" >>> Planning to use %s to make a decision for player %s" % [content, player])
+		match game_logic.decision_info.type:
+			Enums.DecisionType.DecisionType_ForceForEffect:
+				assert_true(game_logic.do_force_for_effect(player, content, false),
+						"%s failed to perform a Force effect using %s" % [player, content])
+			Enums.DecisionType.DecisionType_GaugeForEffect:
+				assert_true(game_logic.do_gauge_for_effect(player, content),
+						"%s failed to perform a Gauge effect using %s" % [player, content])
+			Enums.DecisionType.DecisionType_PayStrikeCost_Required, Enums.DecisionType.DecisionType_PayStrikeCost_CanWild:
+				# There is sometimes an init_extra_cost here
+				# TODO: See if anyone needs it to be more than 0
+				assert_true(game_logic.do_pay_strike_cost(player, content, false),
+						"%s failed to pay a Strike cost using %s" % [player, content])
+			var decision_type:  # Unknown decision type, just roll with it
+				assert_true(game_logic.do_choice(player, content),
+						"Decision of type %s unhandled by test harness (attempted by player %s, content %s)" % [
+								decision_type, player, content])
 
-func execute_strike(initiator, defender, init_card : String, def_card : String, init_choices, def_choices,
-		init_ex = false, def_ex = false, init_force_discard = [], def_force_discard = [], init_extra_cost = 0,
-		init_set_effect_gauge = false, def_set_effect_gauge = false, simul_effect_choices = [], set_choice = -1):
+func process_remaining_decisions(initiator, defender, init_choices, def_choices):
+	var empty_loop_count = 0
+	while init_choices or def_choices:
+		# empty_loop_count is used to detect when there are still prescribed choices in
+		# the input, but the game is not actually making additional decision points available.
+		empty_loop_count += 1
+		if empty_loop_count >= 3:
+			fail_test("Game is not providing decision points to process initiator choices %s or defender choices %s" % [init_choices, def_choices])
+		while game_logic.game_state == Enums.GameState.GameState_PlayerDecision:
+				## TODO: Figure out if it's really necessary to limit ourselves to this decision type
+				# and game_logic.decision_info.type == Enums.DecisionType.DecisionType_ChooseSimultaneousEffect:
+			empty_loop_count = 0  # reset the count each time we actually get into this loop; it is not empty
+			var player = initiator
+			var player_choices = init_choices
+			if game_logic.decision_info.player == defender.my_id:
+				player = defender
+				player_choices = def_choices
+			if not player_choices:
+				fail_test("Insufficient decisions defined for player %s during strike" % player)
+			var choice = player_choices.pop_front()
+			match game_logic.decision_info.type:
+				Enums.DecisionType.DecisionType_ChooseSimultaneousEffect:
+					assert_true(game_logic.do_choice(player, choice),
+							"%s failed to perform a choice with value %s" % [player, choice])
+				Enums.DecisionType.DecisionType_ChooseToDiscard:
+					assert_true(game_logic.do_choose_to_discard(player, choice),
+							"%s failed to discard cards %s" % [player, choice])
+				var decision_type:  # Unknown decision type, just roll with it?
+					assert_true(game_logic.do_choice(player, choice),
+							"Decision of type %s unhandled by test harness (attempted by player %s, content %s)" % [
+									decision_type, player, choice])
+		wait_seconds(0.01)
+
+func execute_strike(initiator, defender, init_card: String, def_card: String,
+		init_ex = false, def_ex = false, init_choices = [], def_choices = []):
 	var all_events = []
-	give_specific_cards(initiator, init_card, defender, def_card)
 
+	## TODO: Because all the card IDs are generated internally and at runtime, it is hard
+	##   to pass those IDs in through *_choices
 	if init_card:
+		var init_card_id = give_player_specific_card(initiator, init_card)
+		var init_card_ex_id = -1
 		if init_ex:
-			give_player_specific_card(initiator, init_card, TestCardId3)
-			do_and_validate_strike(initiator, TestCardId1, TestCardId3)
-		else:
-			do_and_validate_strike(initiator, TestCardId1)
+			init_card_ex_id = give_player_specific_card(initiator, init_card)
+		do_and_validate_strike(initiator, init_card_id, init_card_ex_id)
 	else:
 		do_and_validate_strike(initiator, -1)  # wild swing
+	## TODO: Why no all_events modification in the initiator block? Is it
+	## because `validate` handles the only part we care about?
+	process_decisions(initiator, game_logic.StrikeState.StrikeState_Initiator_SetEffects, init_choices)
 
-	if game_logic.game_state == Enums.GameState.GameState_PlayerDecision and game_logic.active_strike.strike_state == game_logic.StrikeState.StrikeState_Initiator_SetEffects:
-		if init_set_effect_gauge:
-			assert_true(game_logic.do_gauge_for_effect(initiator, init_force_discard), "failed do_gauge_for_effect")
-		elif set_choice > -1:
-			# set_choice only gets used by Waldstein and Mole Knight, so it's a little safer to
-			# guard it behind this condition rather than let it be the default decision method
-			game_logic.do_choice(initiator, set_choice)
-		else:
-			assert_true(game_logic.do_force_for_effect(initiator, init_force_discard, false), "failed do_force_for_effect")
+	if def_card:
+		var def_card_id = give_player_specific_card(defender, def_card)
+		var def_card_ex_id = -1
+		if def_ex:
+			def_card_ex_id = give_player_specific_card(defender, def_card)
+		all_events += do_strike_response(defender, def_card_id, def_card_ex_id)
+	else:
+		all_events += do_strike_response(defender, -1)  # wild swing
+	process_decisions(defender, game_logic.StrikeState.StrikeState_Defender_SetEffects, def_choices)
 
-	if def_ex:
-		give_player_specific_card(defender, def_card, TestCardId4)
-		all_events += do_strike_response(defender, TestCardId2, TestCardId4)
-	elif def_card:
-		all_events += do_strike_response(defender, TestCardId2)
+	process_decisions(initiator, game_logic.StrikeState.StrikeState_Initiator_PayCosts, init_choices)
+	process_decisions(defender, game_logic.StrikeState.StrikeState_Defender_PayCosts, def_choices)
 
-	if game_logic.game_state == Enums.GameState.GameState_PlayerDecision and game_logic.active_strike.strike_state == game_logic.StrikeState.StrikeState_Defender_SetEffects:
-		if def_set_effect_gauge:
-			assert_true(game_logic.do_gauge_for_effect(defender, def_force_discard), "failed defender do_gauge_for_effect")
-		elif set_choice > -1:
-			game_logic.do_choice(defender, set_choice)	
-		else:
-			assert_true(game_logic.do_force_for_effect(defender, def_force_discard, false), "failed defender do_force_for_effect")
-
-	# Pay any costs from gauge
-	if game_logic.active_strike and game_logic.active_strike.strike_state == game_logic.StrikeState.StrikeState_Initiator_PayCosts:
-		var cost = game_logic.active_strike.initiator_card.definition['gauge_cost'] + init_extra_cost
-		var cards = []
-		for i in range(cost):
-			cards.append(initiator.gauge[i].id)
-		game_logic.do_pay_strike_cost(initiator, cards, false)
-
-	# Pay any costs from gauge
-	if game_logic.active_strike and game_logic.active_strike.strike_state == game_logic.StrikeState.StrikeState_Defender_PayCosts:
-		var cost = game_logic.active_strike.defender_card.definition['gauge_cost']
-		var cards = []
-		for i in range(cost):
-			cards.append(defender.gauge[i].id)
-		game_logic.do_pay_strike_cost(defender, cards, false)
-
-	handle_simultaneous_effects(initiator, defender, simul_effect_choices)
-
-	for i in range(init_choices.size()):
-		assert_eq(game_logic.game_state, Enums.GameState.GameState_PlayerDecision, "not in decision for choice 1")
-		assert_true(game_logic.do_choice(initiator, init_choices[i]), "choice 1 failed")
-		handle_simultaneous_effects(initiator, defender, simul_effect_choices)
-	handle_simultaneous_effects(initiator, defender, simul_effect_choices)
-
-	for i in range(def_choices.size()):
-		assert_eq(game_logic.game_state, Enums.GameState.GameState_PlayerDecision, "not in decision for choice 2")
-		assert_true(game_logic.do_choice(defender, def_choices[i]), "choice 2 failed")
-		handle_simultaneous_effects(initiator, defender, simul_effect_choices)
+	process_remaining_decisions(initiator, defender, init_choices, def_choices)
 
 	var events = game_logic.get_latest_events()
 	all_events += events
--- test/unit/test_zangief.gd
+++ test/unit/test_zangief.gd
@@ -9,38 +9,37 @@ func who_am_i():
 
 func test_zangief_crit_power():
 	position_players(player1, 6, player2, 7)
-	give_gauge(player1, 1)
-	give_gauge(player2, 2)
-	give_player_specific_card(player1, "zangief_flyingpowerbomb", TestCardId1)
-	give_player_specific_card(player2, "zangief_atomicsuplex", TestCardId2)
-	assert_true(game_logic.do_strike(player1, TestCardId1, false, -1))
-	assert_true(game_logic.do_gauge_for_effect(player1, [player1.gauge[0].id]))
-	assert_true(game_logic.do_strike(player2, TestCardId2, false, -1))
-	assert_true(game_logic.do_gauge_for_effect(player2, [player2.gauge[0].id]))
+	var p1_gauge = give_gauge(player1, 1)
+	var p2_gauge = give_gauge(player2, 2)
+	execute_strike(player1, player2, "zangief_flyingpowerbomb", "zangief_atomicsuplex",
+			false, false, [p1_gauge], [[p2_gauge[0]]])
+	# Expected: Atomic Suplex swings for 4 + 1 (crit), push 3, close 3 (crit)
+	#           Flying Power Bomb swings (crit stun immune) for 6 + 1 (crit)
 	validate_positions(player1, 3, player2, 4)
 	validate_life(player1, 25, player2, 23)
 
 func test_zangief_exceed_crit():
 	position_players(player1, 4, player2, 7)
-	give_gauge(player1, 4)
-	var card_ids = player1.get_card_ids_in_gauge().slice(1)
-	assert_true(game_logic.do_exceed(player1, card_ids))
-	assert_true(game_logic.do_choice(player1, 1))
+	var p1_gauge = give_gauge(player1, 4)
+	assert_true(game_logic.do_exceed(player1, p1_gauge.slice(1)))
+	assert_true(game_logic.do_choice(player1, 1))  # When you Exceed, pull up to 2
 	validate_positions(player1, 4, player2, 5)
 	# P2 turn
-	give_player_specific_card(player1, "zangief_atomicsuplex", TestCardId1)
-	give_player_specific_card(player2, "standard_normal_cross", TestCardId2)
-	assert_true(game_logic.do_strike(player2, TestCardId2, false, -1))
-	assert_true(game_logic.do_strike(player1, TestCardId1, false, -1))
-	assert_true(game_logic.do_gauge_for_effect(player1, [player1.gauge[0].id]))
+	execute_strike(player2, player1, "standard_normal_cross", "zangief_atomicsuplex", 
+			false, false, [], [[p1_gauge[0]]])
+	# Expected: Atomic Suplex outspeeds Cross (6 + 1 (crit)), hits for 4 + 3 (crit), push 3, close 3 (crit)
 	validate_positions(player1, 7, player2, 8)
 	validate_life(player1, 30, player2, 23)
-	advance_turn(player1)
+
+## Tests of Flying Power Bomb's boost:
+##   Name a range; the opponent must discard a card that includes that range or reveal a hand that
+##   doesn't contain such a card.
+## (Reminder: All test setups are mirror matches.)
 
 func test_zangief_flyingpowerbomb_boost_reveal():
 	position_players(player1, 4, player2, 7)
-	give_player_specific_card(player1, "zangief_flyingpowerbomb", TestCardId1)
-	assert_true(game_logic.do_boost(player1, TestCardId1))
+	var card_id = give_player_specific_card(player1, "zangief_flyingpowerbomb")
+	assert_true(game_logic.do_boost(player1, card_id))
 
 	# Name the range 0-8 are real ranges, 9 is a valid choice but doesn't exist, 10 is X, 11 is -
 	assert_true(game_logic.do_choice(player1, 10)) # Zangief has no X cards.
@@ -50,8 +49,8 @@ func test_zangief_flyingpowerbomb_boost_reveal():
 
 func test_zangief_flyingpowerbomb_boost_reveal2():
 	position_players(player1, 4, player2, 7)
-	give_player_specific_card(player1, "zangief_flyingpowerbomb", TestCardId1)
-	assert_true(game_logic.do_boost(player1, TestCardId1))
+	var card_id = give_player_specific_card(player1, "zangief_flyingpowerbomb")
+	assert_true(game_logic.do_boost(player1, card_id))
 
 	# Name the range 0-8 are real ranges, 9 is a valid choice but doesn't exist, 10 is X, 11 is -
 	assert_true(game_logic.do_choice(player1, 4)) # Zangief has no 4
@@ -61,51 +60,53 @@ func test_zangief_flyingpowerbomb_boost_reveal2():
 
 func test_zangief_flyingpowerbomb_boost_discard_block():
 	position_players(player1, 4, player2, 7)
-	give_player_specific_card(player1, "zangief_flyingpowerbomb", TestCardId1)
-	assert_true(game_logic.do_boost(player1, TestCardId1))
+	var power_bomb_id = give_player_specific_card(player1, "zangief_flyingpowerbomb")
+	assert_true(game_logic.do_boost(player1, power_bomb_id))
 	player2.discard_hand()
-	give_player_specific_card(player2, "standard_normal_block", TestCardId2)
+	var block_id = give_player_specific_card(player2, "standard_normal_block")
 	# Name the range 0-8 are real ranges, 9 is a valid choice but doesn't exist, 10 is X, 11 is -
 	assert_true(game_logic.do_choice(player1, 11))
-	assert_true(game_logic.do_choose_to_discard(player2, [TestCardId2]))
+	assert_true(game_logic.do_choose_to_discard(player2, [block_id]))
 	advance_turn(player2)
 
 func test_zangief_flyingpowerbomb_boost_discard_X():
 	position_players(player1, 4, player2, 7)
-	give_player_specific_card(player1, "zangief_flyingpowerbomb", TestCardId1)
-	assert_true(game_logic.do_boost(player1, TestCardId1))
+	var power_bomb_id = give_player_specific_card(player1, "zangief_flyingpowerbomb")
+	assert_true(game_logic.do_boost(player1, power_bomb_id))
 	player2.discard_hand()
-	give_player_specific_card(player2, "phonon_impulsivefrustration", TestCardId2)
-	# Name the range 0-8 are real ranges, 9 is a valid choice but doesn't exist, 10 is X, 11 is -
+	var frustration_id = give_player_specific_card(player2, "phonon_impulsivefrustration")
+	# Range 2~X; Zangief calls "X" as the range
 	assert_true(game_logic.do_choice(player1, 10))
-	assert_true(game_logic.do_choose_to_discard(player2, [TestCardId2]))
+	assert_true(game_logic.do_choose_to_discard(player2, [frustration_id]))
 	advance_turn(player2)
 
 func test_zangief_flyingpowerbomb_boost_discard_3_with_X_eval():
 	position_players(player1, 4, player2, 7)
-	give_player_specific_card(player1, "zangief_flyingpowerbomb", TestCardId1)
-	assert_true(game_logic.do_boost(player1, TestCardId1))
+	var power_bomb_id = give_player_specific_card(player1, "zangief_flyingpowerbomb")
+	assert_true(game_logic.do_boost(player1, power_bomb_id))
 
 	player2.discard_hand()
-	give_player_specific_card(player2, "phonon_impulsivefrustration", TestCardId2)
-	# Name the range 0-8 are real ranges, 9 is a valid choice but doesn't exist, 10 is X, 11 is -
+	var frustration_id = give_player_specific_card(player2, "phonon_impulsivefrustration")
+	# Range 2~X where X is the attack's Power (printed 3); Zangief calls "3"
 	assert_true(game_logic.do_choice(player1, 3))
-	assert_true(game_logic.do_choose_to_discard(player2, [TestCardId2]))
+	assert_true(game_logic.do_choose_to_discard(player2, [frustration_id]))
 	advance_turn(player2)
 
 func test_zangief_flyingpowerbomb_boost_discard_4_with_X_eval_fails():
 	position_players(player1, 4, player2, 7)
 	advance_turn(player1)
-	give_player_specific_card(player2, "phonon_turningsatisfaction", TestCardId3)
-	assert_true(game_logic.do_boost(player2, TestCardId3))
+	var tuning_id = give_player_specific_card(player2, "phonon_turningsatisfaction")
+	assert_true(game_logic.do_boost(player2, tuning_id))
+	# Continuous boost: +1 Power
 	advance_turn(player2)
 	player1.discard_hand()
-	give_player_specific_card(player1, "zangief_flyingpowerbomb", TestCardId1)
-	assert_true(game_logic.do_boost(player1, TestCardId1))
+	var power_bomb_id = give_player_specific_card(player1, "zangief_flyingpowerbomb")
+	assert_true(game_logic.do_boost(player1, power_bomb_id))
 
 	player2.discard_hand()
-	give_player_specific_card(player2, "phonon_impulsivefrustration", TestCardId2)
-	# Name the range 0-8 are real ranges, 9 is a valid choice but doesn't exist, 10 is X, 11 is -
+	var frustration_id = give_player_specific_card(player2, "phonon_impulsivefrustration")
+	# Range 2~X where X is the attack's Power (printed 3)
+	# Zangief calls "4", which whiffs because the continuous boost only applies during a strike or something
 	assert_true(game_logic.do_choice(player1, 4))
 	var events = game_logic.get_latest_events()
 	validate_has_event(events, Enums.EventType.EventType_RevealHand, player2)
@@ -113,50 +114,52 @@ func test_zangief_flyingpowerbomb_boost_discard_4_with_X_eval_fails():
 
 func test_zangief_flyingpowerbomb_boost_discard_3():
 	position_players(player1, 4, player2, 7)
-	give_player_specific_card(player1, "zangief_flyingpowerbomb", TestCardId1)
-	assert_true(game_logic.do_boost(player1, TestCardId1))
+	var power_bomb_id = give_player_specific_card(player1, "zangief_flyingpowerbomb")
+	assert_true(game_logic.do_boost(player1, power_bomb_id))
 
 	player2.discard_hand()
-	give_player_specific_card(player2, "standard_normal_sweep", TestCardId2)
+	var sweep_id = give_player_specific_card(player2, "standard_normal_sweep")
 	# Name the range 0-8 are real ranges, 9 is a valid choice but doesn't exist, 10 is X, 11 is -
 	assert_true(game_logic.do_choice(player1, 3))
-	assert_true(game_logic.do_choose_to_discard(player2, [TestCardId2]))
+	assert_true(game_logic.do_choose_to_discard(player2, [sweep_id]))
 	advance_turn(player2)
 
 func test_zangief_flyingpowerbomb_boost_discard_kunzite():
 	position_players(player1, 1, player2, 3)
-	give_player_specific_card(player1, "zangief_flyingpowerbomb", TestCardId1)
-	assert_true(game_logic.do_boost(player1, TestCardId1))
+	var power_bomb_id = give_player_specific_card(player1, "zangief_flyingpowerbomb")
+	assert_true(game_logic.do_boost(player1, power_bomb_id))
 
 	player2.discard_hand()
-	give_player_specific_card(player2, "nine_kunzite", TestCardId2)
-	# Name the range 0-8 are real ranges, 9 is a valid choice but doesn't exist, 10 is X, 11 is -
-	assert_true(game_logic.do_choice(player1, 2)) # We're range 2 so kunzite should be 2
-	assert_true(game_logic.do_choose_to_discard(player2, [TestCardId2])) # If this fails, then kunzite didn't match
+	var kunzite_id = give_player_specific_card(player2, "nine_kunzite")
+	# Range X, where X is your range to the opponent
+	assert_true(game_logic.do_choice(player1, 2))
+	assert_true(game_logic.do_choose_to_discard(player2, [kunzite_id]))
 	advance_turn(player2)
+
+## Test Siberian Blizzard passive effect: Opponents can't move past you.
 
 func test_zangief_siberian_move():
 	position_players(player1, 3, player2, 6)
-	give_gauge(player1, 3)
-	give_player_specific_card(player1, "zangief_siberianblizzard", TestCardId1)
-	give_player_specific_card(player2, "standard_normal_dive", TestCardId2)
-	assert_true(game_logic.do_strike(player1, TestCardId1, false, -1))
-	assert_true(game_logic.do_gauge_for_effect(player1, [])) # Skip critical
-	assert_true(game_logic.do_strike(player2, TestCardId2, false, -1))
+	var p1_gauge = give_gauge(player1, 3)
+
 	assert_eq(player1.hand.size(), 5)
-	assert_true(game_logic.do_pay_strike_cost(player1, player1.get_card_ids_in_gauge(), false))
-	assert_eq(player1.hand.size(), 7)
+	execute_strike(player1, player2, "zangief_siberianblizzard", "standard_normal_dive",
+			false, false,
+			[[], p1_gauge],  # Pass on critical payment; pay all gauge for the Ultra
+			[])
+	assert_eq(player1.hand.size(), 7)  # Original 5 + Blizzard - Blizzard + Draw 2 on Hit
+	# Expected: Dive goes first but can't cross over; hits for 5, doesn't stun
+	#           Blizzard hits back for 8
 	validate_positions(player1, 3, player2, 4)
 	validate_life(player1, 25, player2, 22)
-	# Player 2, make sure we can move past them.
+
+	# Verify that player 2 can still cross over outside of the strike
 	assert_true(game_logic.do_move(player2, [player2.hand[0].id, player2.hand[1].id], 2))
 	validate_positions(player1, 3, player2, 2)
-	give_player_specific_card(player1, "standard_normal_sweep", TestCardId3)
-	give_player_specific_card(player2, "standard_normal_dive", TestCardId4)
-	assert_true(game_logic.do_strike(player1, TestCardId3, false, -1))
-	assert_true(game_logic.do_gauge_for_effect(player1, [])) # Skip critical
-	assert_true(game_logic.do_strike(player2, TestCardId4, false, -1))
-	assert_true(game_logic.do_gauge_for_effect(player2, [])) # Skip critical
+
+	# Verify that the movement block doesn't persist to the next strike
+	execute_strike(player1, player2, "standard_normal_sweep", "standard_normal_dive",
+			false, false, [[]], [[]])  # Empty options to decline critical
 	validate_positions(player1, 3, player2, 6)
 	validate_life(player1, 25, player2, 22)
 	advance_turn(player2)
