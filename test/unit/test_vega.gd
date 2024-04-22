f1248b909fc4f27beb3fb0e9020b5576819f5402
Charles Tam <rngesus.wept+github@gmail.com>
2024-04-22 17:53:32 -0400

    Vega update

--- test/unit/test_vega.gd
+++ test/unit/test_vega.gd
@@ -88,103 +88,117 @@ func test_vega_bloodyhigh_3_spaces_playerleft():
 func test_vega_scarletterror_boost():
 	position_players(player1, 1, player2, 6)
 	var terror_id = give_player_specific_card(player1, "vega_scarletterror")
-	assert_true(game_logic.do_boost(player1, terror_id, player1.hand.slice(0, 2)))
-	assert_true(game_logic.do_choice(player1, get_choice_index_for_position(5)))
+	assert_true(game_logic.do_boost(player1, terror_id, get_cards_from_hand(player1, 2)))
+	assert_true(game_logic.do_choice(player1, select_space(5)))
 	validate_positions(player1, 5, player2, 6)
-	give_gauge(player1, 4)
-	execute_strike(player1, player2, "vega_bloodyhighclaw", "standard_normal_focus", [], [], false, false, [player1.gauge[0].id], [], 0, true, false)
-	# Move up to 8
-	assert_true(game_logic.do_choice(player1, 0))
+
+	var p1_gauge = give_gauge(player1, 3)
+	execute_strike(player1, player2, "vega_bloodyhighclaw", "standard_normal_focus",
+			false, false, [[], p1_gauge, 0], [])  # Decline Critical, pay for Ultra, decline move
+	# Expected: Bloody High Claw hits for
+	#     4 + 3 (3 spaces behind opp.) + 2 (Scarlet Terror gets ability bonus) - 2 (Focus Armor) = 7
 	validate_positions(player1, 5, player2, 6)
 	validate_life(player1, 30, player2, 23)
 	advance_turn(player2)
 
 
-# func test_vega_scarletterror_boost_exceeded():
-# 	position_players(player1, 1, player2, 6)
-# 	player1.exceeded = true
-# 	give_player_specific_card(player1, "vega_scarletterror", TestCardId3)
-# 	assert_true(game_logic.do_boost(player1, TestCardId3, [player1.hand[0].id, player1.hand[1].id]))
-# 	assert_true(game_logic.do_choice(player1, get_choice_index_for_position(5)))
-# 	validate_positions(player1, 5, player2, 6)
-# 	give_gauge(player1, 4)
-# 	execute_strike(player1, player2, "vega_bloodyhighclaw", "standard_normal_cross", [], [], false, true, [player1.gauge[0].id], [], 0, true, false)
-# 	# Move up to 8
-# 	assert_true(game_logic.do_choice(player1, 0))
-# 	validate_positions(player1, 5, player2, 6)
-# 	# Total power of 4 + 3 + ua 3 = 10, -1 armor for ex = 21
-# 	validate_life(player1, 30, player2, 21)
-# 	advance_turn(player2)
+func test_vega_scarletterror_boost_exceeded():
+	position_players(player1, 1, player2, 6)
+	player1.exceed()
+	var terror_id = give_player_specific_card(player1, "vega_scarletterror")
+	assert_true(game_logic.do_boost(player1, terror_id, get_cards_from_hand(player1, 2)))
+	assert_true(game_logic.do_choice(player1, select_space(5)))
+	validate_positions(player1, 5, player2, 6)
+
+	var p1_gauge = give_gauge(player1, 3)
+	execute_strike(player1, player2, "vega_bloodyhighclaw", "standard_normal_cross",
+			false, true, [[], p1_gauge, 0], [])  # Decline Critical, pay for Ultra, decline move
+	# Move up to 8
+	validate_positions(player1, 5, player2, 6)
+	# Total power of 4 + 3 + ua 3 = 10, -1 armor for ex = 21
+	validate_life(player1, 30, player2, 21)
+	advance_turn(player2)
 
 
-# func test_vega_scarletterror_boost_exceeded_no_difference_edge():
-# 	position_players(player1, 1, player2, 7)
-# 	player1.exceeded = true
-# 	give_player_specific_card(player1, "vega_scarletterror", TestCardId3)
-# 	assert_true(game_logic.do_boost(player1, TestCardId3, [player1.hand[0].id, player1.hand[1].id]))
-# 	assert_true(game_logic.do_choice(player1, get_choice_index_for_position(9)))
-# 	validate_positions(player1, 9, player2, 7)
-# 	give_gauge(player1, 4)
-# 	execute_strike(player1, player2, "vega_bloodyhighclaw", "standard_normal_cross", [], [], false, true, [player1.gauge[0].id], [], 0, true, false)
-# 	# Move up to 8
-# 	assert_true(game_logic.do_choice(player1, 0))
-# 	validate_positions(player1, 9, player2, 7)
-# 	# Total power of 4 + 3 + ua 3 = 10, -1 armor for ex = 21
-# 	validate_life(player1, 30, player2, 21)
-# 	advance_turn(player2)
+func test_vega_scarletterror_boost_exceeded_no_difference_edge():
+	position_players(player1, 1, player2, 7)
+	player1.exceed()
+	var terror_id = give_player_specific_card(player1, "vega_scarletterror")
+	assert_true(game_logic.do_boost(player1, terror_id, get_cards_from_hand(player1, 2)))
+	assert_true(game_logic.do_choice(player1, select_space(9)))
+	validate_positions(player1, 9, player2, 7)
+
+	var p1_gauge = give_gauge(player1, 3)
+	execute_strike(player1, player2, "vega_bloodyhighclaw", "standard_normal_cross",
+			false, true, [[], p1_gauge, 0], [])  # Decline Critical, pay for Ultra, decline move
+	# Move up to 8
+	validate_positions(player1, 9, player2, 7)
+	# Expected: Total power of 4 + 3 + ua 3 = 10, -1 armor for ex = 21
+	#     i.e. checking that the UA doesn't apply twice (once from UA, once from Terror)
+	validate_life(player1, 30, player2, 21)
+	advance_turn(player2)
 
 
-# func test_vega_scarletterror_boost_default_edge_normal():
-# 	position_players(player1, 1, player2, 7)
-# 	give_player_specific_card(player1, "vega_scarletterror", TestCardId3)
-# 	assert_true(game_logic.do_boost(player1, TestCardId3, [player1.hand[0].id, player1.hand[1].id]))
-# 	assert_true(game_logic.do_choice(player1, get_choice_index_for_position(9)))
-# 	validate_positions(player1, 9, player2, 7)
-# 	execute_strike(player1, player2, "standard_normal_cross", "standard_normal_cross", [], [], false, false, [], [], 0, false, false)
-# 	validate_positions(player1, 9, player2, 7)
-# 	validate_life(player1, 30, player2, 25)
-# 	advance_turn(player2)
+func test_vega_scarletterror_boost_default_edge_normal():
+	position_players(player1, 1, player2, 7)
+	var terror_id = give_player_specific_card(player1, "vega_scarletterror")
+	assert_true(game_logic.do_boost(player1, terror_id, get_cards_from_hand(player1, 2)))
+	assert_true(game_logic.do_choice(player1, select_space(9)))
+	validate_positions(player1, 9, player2, 7)
 
-# func test_vega_scarletterror_boost_exceeded_edge_normal():
-# 	position_players(player1, 1, player2, 7)
-# 	player1.exceeded = true
-# 	give_player_specific_card(player1, "vega_scarletterror", TestCardId3)
-# 	assert_true(game_logic.do_boost(player1, TestCardId3, [player1.hand[0].id, player1.hand[1].id]))
-# 	assert_true(game_logic.do_choice(player1, get_choice_index_for_position(9)))
-# 	validate_positions(player1, 9, player2, 7)
-# 	execute_strike(player1, player2, "standard_normal_assault", "standard_normal_cross", [], [], false, false, [], [], 0, false, false)
-# 	validate_positions(player1, 8, player2, 7)
-# 	validate_life(player1, 30, player2, 23)
-# 	advance_turn(player1)
+	execute_strike(player1, player2, "standard_normal_cross", "standard_normal_cross")
+	validate_positions(player1, 9, player2, 7)
+	# Expected: Scarlet Terror causes the UA power bonus to apply to Cross even
+	#     though it's a Normal
+	validate_life(player1, 30, player2, 25)
+	advance_turn(player2)
 
+func test_vega_scarletterror_boost_exceeded_edge_normal():
+	position_players(player1, 1, player2, 7)
+	player1.exceed()
+	var terror_id = give_player_specific_card(player1, "vega_scarletterror")
+	assert_true(game_logic.do_boost(player1, terror_id, get_cards_from_hand(player1, 2)))
+	assert_true(game_logic.do_choice(player1, select_space(9)))
+	validate_positions(player1, 9, player2, 7)
 
-# func test_vega_skyhighclaw_boost_miss():
-# 	position_players(player1, 1, player2, 7)
-# 	give_player_specific_card(player1, "vega_bloodyhighclaw", TestCardId1)
-# 	player1.discard_hand()
-# 	player1.draw(3)
-# 	give_player_specific_card(player1, "vega_skyhighclaw", TestCardId2)
-# 	assert_true(game_logic.do_boost(player1, TestCardId2, [player1.hand[0].id]))
-# 	# Advance or retreat 3.
-# 	assert_true(game_logic.do_choice(player1, 0)) # Advance 3
-# 	validate_positions(player1, 4, player2, 7)
-# 	advance_turn(player2)
-# 	assert_eq(player1.continuous_boosts.size(), 0)
+	execute_strike(player1, player2, "standard_normal_assault", "standard_normal_cross")
+	# Expected: Scarlet Terror causes the UA power and speed bonuses to apply to
+	#     Assault even though it's a Normal
+	validate_positions(player1, 8, player2, 7)
+	validate_life(player1, 30, player2, 23)
+	advance_turn(player1)  # P1 has advantage
 
-# func test_vega_skyhighclaw_boost_doit():
-# 	position_players(player1, 5, player2, 7)
-# 	give_player_specific_card(player1, "vega_bloodyhighclaw", TestCardId1)
-# 	player1.discard_hand()
-# 	player1.draw(3)
-# 	give_player_specific_card(player1, "vega_skyhighclaw", TestCardId2)
-# 	assert_true(game_logic.do_boost(player1, TestCardId2, [player1.hand[0].id]))
-# 	# Advance or retreat 3.
-# 	assert_true(game_logic.do_choice(player1, 0)) # Advance 3
-# 	validate_positions(player1, 9, player2, 7)
-# 	assert_true(game_logic.do_boost(player1, TestCardId1))
-# 	assert_eq(player1.continuous_boosts.size(), 1)
-# 	assert_eq(player1.continuous_boosts[0].id, TestCardId1)
-# 	advance_turn(player2)
+## Sky High Claw boost -- (1 Force) Move 3. If you moved past the opponent, put
+##     a Continuous Boost from your discard into play.
+
+func test_vega_skyhighclaw_boost_miss():
+	position_players(player1, 1, player2, 7)
+	var bhc_id = give_player_specific_card(player1, "vega_bloodyhighclaw")
+	player1.discard_hand()
+	player1.draw(3)
+
+	var shc_id = give_player_specific_card(player1, "vega_skyhighclaw")
+	assert_true(game_logic.do_boost(player1, shc_id, get_cards_from_hand(player1, 1)))
+	assert_true(game_logic.do_choice(player1, 0)) # Advance 3
+	validate_positions(player1, 4, player2, 7)
+	# No further action since Vega didn't move past the opponent
+	advance_turn(player2)
+	assert_eq(player1.continuous_boosts.size(), 0)
+
+func test_vega_skyhighclaw_boost_doit():
+	position_players(player1, 5, player2, 7)
+	var bhc_id = give_player_specific_card(player1, "vega_bloodyhighclaw")
+	player1.discard_hand()
+	player1.draw(3)
+
+	var shc_id = give_player_specific_card(player1, "vega_skyhighclaw")
+	assert_true(game_logic.do_boost(player1, shc_id, get_cards_from_hand(player1, 1)))
+	assert_true(game_logic.do_choice(player1, 0)) # Advance 3
+	validate_positions(player1, 9, player2, 7)
+	assert_true(game_logic.do_boost(player1, bhc_id))  # P1 is allowed to boost now
+	assert_eq(player1.continuous_boosts.size(), 1)
+	assert_eq(player1.continuous_boosts[0].id, bhc_id)
+	advance_turn(player2)  # No further action from P1 (does not have to pay BHC Force costs)
 
 # func test_vega_rollingcrystalflash_boost_miss():
 # 	position_players(player1, 1, player2, 7)
