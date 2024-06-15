00e804ff367ac7fdc4ace87195f80dc241e0e0f7
danielktaylor <daniel-k-taylor@users.noreply.github.com>
2024-06-15 12:07:30 -0700

    Add extra akuma test involving opponent powerup boost

--- globals/global_settings.gd
+++ globals/global_settings.gd
@@ -5,7 +5,7 @@ signal settings_loaded
 const ReleaseLoggingEnabled = false # If true, log even on release builds.
 const UseAzureServerAlways = true # If true, always defaults to the azure server.
 var MuteEmotes = false
-const ClientVersionString : String = "240613.1900" # YYMMDD.HHMM
+const ClientVersionString : String = "240615.1200" # YYMMDD.HHMM
 
 const CharacterBanlist = ['carmine']
 # All times are in seconds
