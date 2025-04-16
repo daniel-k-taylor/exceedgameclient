062dd90ceb92a2879a2312f7fe7b2a96646ebd4d
danielktaylor <daniel-k-taylor@users.noreply.github.com>
2025-04-15 19:10:12 -0700

    Update version

--- globals/global_settings.gd
+++ globals/global_settings.gd
@@ -5,7 +5,7 @@ signal settings_loaded
 const ReleaseLoggingEnabled = false # If true, log even on release builds.
 const UseAzureServerAlways = true # If true, always defaults to the azure server.
 var MuteEmotes = false
-const ClientVersionString : String = "250412.1040" # YYMMDD.HHMM
+const ClientVersionString : String = "250415.1910" # YYMMDD.HHMM
 const ReplayVersion : int = 1
 
 const CharacterBanlist = ['carmine']
