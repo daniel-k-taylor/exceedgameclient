2e55800a1cfb1fd38ad2add8a9a2085b3b6b0d08
danielktaylor <daniel-k-taylor@users.noreply.github.com>
2025-05-14 20:33:21 -0700

    Set default azure server usage back to true

--- globals/global_settings.gd
+++ globals/global_settings.gd
@@ -3,7 +3,7 @@ extends Node
 signal settings_loaded
 
 const ReleaseLoggingEnabled = false # If true, log even on release builds.
-const UseAzureServerAlways = false # If true, always defaults to the azure server.
+const UseAzureServerAlways = true # If true, always defaults to the azure server.
 var MuteEmotes = false
 const ClientVersionString : String = "250514.2010" # YYMMDD.HHMM
 const ReplayVersion : int = 1
