0802e50e408fd006008b339590b1bcf3b9887662
Sompursone <jackcarson2020@gmail.com>
2025-06-08 20:07:06 -0400

    Changed to include custom deck in this file rather than from decks/data/test_custom.json

--- test/unit/test_custom_character.gd
+++ test/unit/test_custom_character.gd
@@ -1,7 +1,128 @@
 extends ExceedGutTest
 
 func who_am_i():
-	return "test_custom"
+	var character_identity = {
+		"id": "test_custom",
+		"season": 1,
+		"display_name": "Custom character element tester",
+		"set_starting_face_attack": true,
+		"starting_face_attack_id": "standard_normal_dive",
+		"exceed_cost": 2,
+		"ability_effects": [],
+		"exceed_ability_effects": [],
+		"image_resources": {
+			"character_default": {
+				"url": "https://i.imgur.com/crrqVvH.jpeg",
+				"multiple_cards": false
+			},
+			"character_exceeded": {
+				"url": "https://i.imgur.com/rfrBEIx.jpeg",
+				"multiple_cards": false
+			},
+			"cardback": {
+				"url": "https://i.imgur.com/s6wpKBq.jpeg",
+				"multiple_cards": false
+			},
+			"specials": {
+				"url": "https://i.imgur.com/TTbuUIL.jpeg",
+				"multiple_cards": true
+			},
+			"normals": {
+				"url": "https://i.imgur.com/tXuqP40.jpeg",
+				"multiple_cards": true
+			}
+		},
+		"cards": [
+			{
+				"set_aside": true,
+				"hide_from_reference": true,
+				"definition_id": "standard_normal_dive",
+				"image_name": "normals",
+				"image_index": 3
+			},
+			{
+				"definition_id": "standard_normal_grasp",
+				"image_name": "normals",
+				"image_index": 0
+			},
+			{
+				"definition_id": "standard_normal_grasp",
+				"image_name": "normals",
+				"image_index": 0
+			},
+			{
+				"definition_id": "standard_normal_cross",
+				"image_name": "normals",
+				"image_index": 1
+			},
+			{
+				"definition_id": "standard_normal_cross",
+				"image_name": "normals",
+				"image_index": 1
+			},
+			{
+				"definition_id": "standard_normal_assault",
+				"image_name": "normals",
+				"image_index": 2
+			},
+			{
+				"definition_id": "standard_normal_assault",
+				"image_name": "normals",
+				"image_index": 2
+			},
+			{
+				"definition_id": "standard_normal_dive",
+				"image_name": "normals",
+				"image_index": 3
+			},
+			{
+				"definition_id": "standard_normal_dive",
+				"image_name": "normals",
+				"image_index": 3
+			},
+			{
+				"definition_id": "standard_normal_spike",
+				"image_name": "normals",
+				"image_index": 4
+			},
+			{
+				"definition_id": "standard_normal_spike",
+				"image_name": "normals",
+				"image_index": 4
+			},
+			{
+				"definition_id": "standard_normal_sweep",
+				"image_name": "normals",
+				"image_index": 5
+			},
+			{
+				"definition_id": "standard_normal_sweep",
+				"image_name": "normals",
+				"image_index": 5
+			},
+			{
+				"definition_id": "standard_normal_focus",
+				"image_name": "normals",
+				"image_index": 6
+			},
+			{
+				"definition_id": "standard_normal_focus",
+				"image_name": "normals",
+				"image_index": 6
+			},
+			{
+				"definition_id": "standard_normal_block",
+				"image_name": "normals",
+				"image_index": 7
+			},
+			{
+				"definition_id": "standard_normal_block",
+				"image_name": "normals",
+				"image_index": 7
+			}
+		]
+	}
+	return character_identity
 
 ##
 ## Tests start here
