2de2ed24026a3a9d7c09f96e7823ac1c8551fbe7
Serp <rocktalonttaylor@gmail.com>
2026-08-31 01:19:43 -0400

    Doomies effects

--- globals/game_strings.gd
+++ globals/game_strings.gd
@@ -1308,7 +1308,10 @@ func get_effect_type_text(effect, card_name_source : String = "", char_effect_pa
 		StrikeEffects.PowerupPerBoostInPlay:
 			effect_str += "+" + str(effect['amount']) + " Power per boost in play."
 		StrikeEffects.PowerupPerCardInHand:
-			effect_str += "+" + str(effect['amount']) + " Power per card in hand up to " + str(effect['amount_max']) + "."
+			effect_str += "+" + str(effect['amount']) + " Power per card in hand"
+			if 'amount_max' in effect:
+				effect_str += " up to " + str(effect['amount_max'])
+			effect_str += "."
 		StrikeEffects.PowerupPerCardInOpponentHand:
 			var every_str = "card"
 			if 'per_card' in effect:
@@ -1409,6 +1412,8 @@ func get_effect_type_text(effect, card_name_source : String = "", char_effect_pa
 			effect_str += "Push to attack's max range"
 		StrikeEffects.PushToRange:
 			effect_str += "Push to range %s" % effect['amount']
+		StrikeEffects.PushDamageDealt:
+			effect_str += "Push equal to damage dealt"
 		StrikeEffects.RangeIncludesIfMovedPast:
 			effect_str += "If you move past the opponent, your range includes them"
 		StrikeEffects.Rangeup:
